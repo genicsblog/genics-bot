6758617d85212e408e01602c44e9262e9544a6b4
Gourav Khunger <gouravkhunger18@gmail.com>
2022-02-06 23:40:05 +0530

    Complete migration from zapier to cron job (#2)
    * Complete migration from zapier to cron job
    
    * Fix indentation

--- deploy.js
+++ deploy.js
@@ -11,8 +11,8 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
---
+++ util/log-analytics.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const { google } = require('googleapis')
+const prettySeconds = require('pretty-seconds')
+const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
+
+const jwt = new google.auth.JWT(process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL, null, process.env.GOOGLE_ANALYTICS_PRIVATE_KEY, scopes)
+
+async function getData(dateString) {
+    const result = await google.analytics('v3').data.ga.get({
+        'auth': jwt,
+        'ids': `ga:${process.env.GOOGLE_ANALYTICS_VIEW_ID}`,
+        'start-date': dateString,
+        'end-date': dateString,
+        'metrics': 'ga:users,ga:newUsers,ga:pageviews,ga:uniquePageviews,ga:sessions,ga:percentNewSessions,ga:avgTimeOnPage,ga:bounceRate'
+    })
+
+    return result.data.totalsForAllResults
+}
+
+module.exports = async (client) => {
+    client.channels.cache.get(process.env.TESTING_CHANNEL_ID).send("Time to log analytics data...")
+
+    const authorizeJWT = await jwt.authorize()
+    const data = await getData(dateString)
+
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const dateString = yesterday.toISOString().split('T')[0]
+
+    const prettyDate = yesterday.toLocaleDateString("en-US", {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    })
+
+    const message = `
+Hey genics community! Here are the blog's stats for **${prettyDate}**:
+
+⦁ There were a total of **${data["ga:pageviews"]} pageviews** out of which **${data["ga:uniquePageviews"]}** were unique!
+
+⦁ A total of **${data["ga:users"]} viewers** visited genics today, out of which **${data["ga:newUsers"]}** were new to the site!
+
+⦁ The average time spent on a page was **${prettySeconds(parseFloat(data["ga:avgTimeOnPage"]))}** and the bounce rate was **${parseFloat(data["ga:bounceRate"]).toFixed(2)}%**!
+
+⦁ There were **${data["ga:sessions"]} sessions** today, out of which **${parseFloat(data["ga:percentNewSessions"]).toFixed(2)}%** were new!
+
+I hope you find this data beneficial :)
+`
+    client.channels.cache.get(process.env.ANALYTICS_CHANNEL_ID).send(message)
+}
