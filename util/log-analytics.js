4da5643fc1e35f4363fc6b1d631290fa6497603e
Gourav Khunger <gouravkhunger18@gmail.com>
2022-04-05 14:18:01 +0530

    Update to ga4 APIs

--- util/log-analytics.js
+++ util/log-analytics.js
@@ -1,49 +1,79 @@
 require('dotenv').config()
+const axios = require('axios')
 const { google } = require('googleapis')
 const prettySeconds = require('pretty-seconds')
 const scopes = 'https://www.googleapis.com/auth/analytics.readonly'
 
 const jwt = new google.auth.JWT(process.env.GOOGLE_ANALYTICS_CLIENT_EMAIL, null, process.env.GOOGLE_ANALYTICS_PRIVATE_KEY.replace(/\\n/g, '\n'), scopes)
 
-async function getData(dateString) {
-	const result = await google.analytics('v3').data.ga.get({
-		'auth': jwt,
-		'ids': `ga:${process.env.GOOGLE_ANALYTICS_VIEW_ID}`,
-		'start-date': dateString,
-		'end-date': dateString,
-		'metrics': 'ga:users,ga:newUsers,ga:pageviews,ga:uniquePageviews,ga:sessions,ga:percentNewSessions,ga:avgTimeOnPage,ga:bounceRate'
-	})
+const body = {
+    "dateRanges": [
+      {
+        "startDate": "yesterday",
+        "endDate": "yesterday"
+      }
+    ],
+    "metrics": [
+      {
+        "name": "activeUsers",
+        "invisible": false
+      }, {
+        "name": "newUsers",
+        "invisible": false 
+      }, {
+        "name": "screenPageViews",
+        "invisible": false
+      }, {
+        "name": "sessions",
+        "invisible": false
+      },{
+        "name": "averageSessionDuration",
+        "invisible": false
+      }, {
+        "name": "bounceRate",
+        "invisible": false
+      }
+    ]
+}
 
-	return result.data.totalsForAllResults
+const url = `https://analyticsdata.googleapis.com/v1beta/${process.env.GOOGLE_ANALYTICS_VIEW_ID}:runReport`
+
+async function getData(access_token) {
+    const result = await axios.post(url, body, {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        }
+    })
+
+    return result.data.rows[0].metricValues
 }
 
 module.exports = async (client) => {
-	client.channels.cache.get(process.env.TESTING_CHANNEL_ID).send("Time to log analytics data...")
+    client.channels.cache.get(process.env.TESTING_CHANNEL_ID).send("Time to log analytics data...")
 
-	let date = new Date()
-	const dateString = date.toISOString().split('T')[0] // YYYY-MM-DD format
+    let date = new Date(Date.now() - 864e5)
 
-	const prettyDate = date.toLocaleDateString("en-US", {
-		year: 'numeric',
-		month: 'short',
-		day: 'numeric'
-	})
+    const prettyDate = date.toLocaleDateString("en-US", {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    })
 
-	const authorizeJWT = await jwt.authorize()
-	const data = await getData(dateString)
+    const authorizeJWT = await jwt.authorize()
+    const data = await getData(authorizeJWT.access_token)
 
-	const message = `
+    const message = `
 Hey genics community! Here are the blog's stats for **${prettyDate}**:
 
-⦁ There were a total of **${data["ga:pageviews"]} pageviews** out of which **${data["ga:uniquePageviews"]}** were unique!
+⦁ There were a total of **${data[2].value} pageviews**!
 
-⦁ A total of **${data["ga:users"]} viewers** visited genics today, out of which **${data["ga:newUsers"]}** were new to the site!
+⦁ A total of **${data[0].value} viewers** visited genics, out of which **${data[1].value}** were new to the site!
 
-⦁ The average time spent on a page was **${prettySeconds(parseFloat(data["ga:avgTimeOnPage"]))}** and the bounce rate was **${parseFloat(data["ga:bounceRate"]).toFixed(2)}%**!
+⦁ There were **${data[3].value} sessions** and the average session duration was **${prettySeconds(parseFloat(data[4].value))}**!
 
-⦁ There were **${data["ga:sessions"]} sessions** today, out of which **${parseFloat(data["ga:percentNewSessions"]).toFixed(2)}%** were new!
+⦁ The bounce rate was **${(parseFloat(data[5].value) * 100.00).toFixed(2)}%**
 
 I hope you find this data beneficial :)
 `
-	client.channels.cache.get(process.env.ANALYTICS_CHANNEL_ID).send(message)
+    client.channels.cache.get(process.env.ANALYTICS_CHANNEL_ID).send(message)
 }
