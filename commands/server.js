b4b92175d1979334ff4c89465f4c9168889a8095
Rafael B. Pires <86871991+bpires@users.noreply.github.com>
2022-10-07 02:55:50 -0300

    chore: address indentation issues (#4)
    Co-authored-by: Gourav Khunger <gouravkhunger18@gmail.com>

--- commands/server.js
+++ commands/server.js
@@ -2,35 +2,34 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 const pendingReview = require('../util/pending-review')
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Replies with the server info')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('info')
-				.setDescription('info about server'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('icon')
-				.setDescription('icon of server'))
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('get-review-articles')
-				.setDescription('Gets a list of articles that need review from @Reviewer')),
+  data: new SlashCommandBuilder()
+    .setName('server')
+    .setDescription('Replies with the server info')
+    .addSubcommand(subcommand =>
+      subcommand.setName('info').setDescription('info about server')
+    )
+    .addSubcommand(subcommand =>
+      subcommand.setName('icon').setDescription('icon of server')
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('get-review-articles')
+        .setDescription(
+          'Gets a list of articles that need review from @Reviewer'
+        )
+    ),
 
-	async execute(interaction, client) {
-		if (interaction.options.getSubcommand() === 'info') {
-			await interaction.reply(
-				`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
-			)
-		}
-		else if (interaction.options.getSubcommand() === 'icon') {
-			await interaction.reply(
-				interaction.guild.iconURL({ size: 1024, dynamic: true })
-			)
-		}
-		else if (interaction.options.getSubcommand() === 'get-review-articles') {
-			pendingReview(client, true)
-		}
-	},
+  async execute(interaction, client) {
+    if (interaction.options.getSubcommand() === 'info') {
+      await interaction.reply(
+        `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`
+      )
+    } else if (interaction.options.getSubcommand() === 'icon') {
+      await interaction.reply(
+        interaction.guild.iconURL({ size: 1024, dynamic: true })
+      )
+    } else if (interaction.options.getSubcommand() === 'get-review-articles') {
+      pendingReview(client, true)
+    }
+  }
 }
--- deploy.js
+++ deploy.js
@@ -8,15 +8,18 @@ const guildId = process.env.GUILD_ID
 const token = process.env.BOT_TOKEN
 
 const commands = []
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	commands.push(command.data.toJSON())
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
 }
 
 const rest = new REST({ version: '9' }).setToken(token)
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error)
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error)
